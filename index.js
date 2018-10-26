f2f8998b2812f110591a937c54a8fab9bbdc6bf4
Zack <zack@fbts.ca>
2018-10-26 17:05:13 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-var require = require;var args = process.argv;const fs = require("fs");function bufferFile(relPath) {return fs.readFileSync(path.join(__dirname, relPath)).toString();}if (args[2]) {var fileName = args[2];var data = bufferFile(fileName);console.log(eval(data));};
+var require = require;
+var args = process.argv;
+const fs = require("fs");
+
+function bufferFile(relPath) {
+  return fs.readFileSync(path.join(__dirname, relPath)).toString();
+};
+
+function execute(command) {
+  const exec = require('child_process').exec;
+
+  exec(command, (err, stdout, stderr) => {
+    process.stdout.write(stdout);
+  });
+};
+
+class object {
+  constructor() {
+    this.data = {}
+  };
+  function set(key,val) {
+    this.data[key] = val;
+  };
+  function get(key) {
+    return this.data[key];
+  };
+};
+
+module.exports = function(fileName)
+  var file = bufferFile(fileName);
+  let dat = new Function("stdout.write","object",fileName);
+  return dat(process.stdout.write,object);
+};
